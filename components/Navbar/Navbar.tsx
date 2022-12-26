a41d08aa92d48e69e61e890179504f6d1f45fe1c
mussiDev <joakoomussi@gmail.com>
2022-12-26 14:28:33 -0300

    navbar mobile

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,26 +2,26 @@ import React, { useState } from "react";
 import navbarItem from "../../common/navbarItem";
 import NavbarItem from "./NavbarItem";
 import items from "../../api/navbarItems.json";
-import { FaDev } from "react-icons/fa";
+import { FaDev, FaGithub, FaLinkedin } from "react-icons/fa";
 import Link from "next/link";
 const Navbar = () => {
 	return (
-		<header className='fixed z-20 w-full p-2 text-lg backdrop-blur-md'>
-			<div className='max-w-3xl mx-auto'>
-				<nav className='flex items-center gap-3'>
-					<Link
-						href='/'
-						className='flex items-center gap-2 p-2 text-lg font-semibold tracking-tighter'>
-						<FaDev className='' />
-						Joaquín Mussi
-					</Link>
-					<ul className='items-center hidden gap-6 lg:flex'>
-						{items &&
-							items.map((item: navbarItem, k: number) => (
-								<NavbarItem key={k} link={item.link} text={item.text} />
-							))}
-					</ul>
-				</nav>
+		<header className='fixed z-20 flex items-center justify-between w-full gap-10 p-2 text-lg'>
+			<Link
+				href='/'
+				className='flex items-center gap-2 text-lg font-semibold tracking-tighter'>
+				<FaDev className='' />
+				Joaquín Mussi
+			</Link>
+			<ul className='items-center hidden gap-6 p-2 mx-auto lg:flex'>
+				{items &&
+					items.map((item: navbarItem, k: number) => (
+						<NavbarItem key={k} link={item.link} text={item.text} />
+					))}
+			</ul>
+			<div className='flex gap-2 lg:hidden'>
+				<FaGithub />
+				<FaLinkedin />
 			</div>
 		</header>
 	);
