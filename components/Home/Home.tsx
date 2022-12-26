9bf7c37d4afd41e785dd438df3c356ccc49a3d6a
mussiDev <joakoomussi@gmail.com>
2022-12-26 13:04:02 -0300

    add new styles

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -3,7 +3,7 @@ import Navbar from "../Navbar/Navbar";
 
 const Home = () => {
 	return (
-		<div className=' bg-slate-800 '>
+		<div className='min-h-screen bg-slate-800 min-w-screen'>
 			<Navbar />
 		</div>
 	);
--- components/Navbar/NavbarItem.tsx
+++ components/Navbar/NavbarItem.tsx
@@ -5,7 +5,7 @@ import navbarItem from "../../common/navbarItem";
 const NavbarItem = (props: navbarItem) => {
 	const { link, text } = props;
 	return (
-		<li>
+		<li className='hover:text-orange-600'>
 			<Link href={link}>{text}</Link>
 		</li>
 	);
