5925bffee6b03ad398e33547363dd1c36fd9e03c
mussiDev <joakoomussi@gmail.com>
2022-12-26 20:05:23 -0300

    add class container

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -4,7 +4,7 @@ import Navbar from "../Navbar/Navbar";
 
 const Home = () => {
 	return (
-		<div className='min-h-screen text-white max-w-screen bg-slate-800'>
+		<div className='container'>
 			<Navbar />
 			<Main />
 		</div>
