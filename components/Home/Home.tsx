3194b56413926fe3222f47c459693c8ce6751e17
mussiDev <joakoomussi@gmail.com>
2022-12-26 14:38:20 -0300

    new component main

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Main from "../Main/Main";
 import Navbar from "../Navbar/Navbar";
 
 const Home = () => {
 	return (
 		<div className='min-h-screen text-white max-w-screen bg-slate-800'>
 			<Navbar />
+			<Main />
 		</div>
 	);
 };
