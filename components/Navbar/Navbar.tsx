0e55d9cb86ef7817fe00c79dc0724410e58c6881
mussiDev <joakoomussi@gmail.com>
2023-01-01 20:08:27 -0300

    fix responsive navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -8,20 +8,18 @@ import navbarItem from "../../entities/navbarItem";
 
 const Navbar = () => {
 	return (
-		<header className='fixed z-20 flex items-center justify-between w-full gap-10 p-2 px-10 text-lg backdrop-blur-md bg-slate-800'>
-			<Link
-				href='/'
-				className='flex items-center gap-2 text-lg font-semibold tracking-tighter'>
+		<header className='fixed z-[100] shadow-xl flex justify-between w-full h-16 px-10 text-lg backdrop-blur-md bg-slate-800'>
+			<Link href='/' className='flex items-center gap-2 font-semibold'>
 				<FaDev className='cursor-pointer' />
 				Joaquín Mussi
 			</Link>
-			<ul className='items-center hidden gap-6 p-2 mx-auto lg:flex'>
+			<ul className='hidden gap-6 lg:flex items-center'>
 				{items &&
 					items.map((item: navbarItem, k: number) => (
 						<NavbarItem key={k} link={item.link} text={item.text} />
 					))}
 			</ul>
-			<div className='flex gap-2 cursor-pointer lg:hidden'>
+			<div className='flex gap-2 cursor-pointer lg:hidden items-center'>
 				<IconLink to='https://github.com/MussiDev' icon='github' />
 				<IconLink
 					to='https://www.linkedin.com/in/joaquinmussi/'
