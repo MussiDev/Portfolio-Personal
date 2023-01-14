1e39e6cc35ffddf2962372a6dbdfd6d160088183
joakom24 <joakoomussi@gmail.com>
2023-01-14 17:29:40 -0300

    add projects items

--- common/ProjectItem.tsx
+++ common/ProjectItem.tsx
@@ -6,23 +6,17 @@ import projectItem from "../entities/projectItem";
 const ProjectItem = (props: projectItem) => {
 	const { title, backgroundImg, tech, projectUrl } = props;
 	return (
-		<div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
+		<div className='flex flex-col items-center gap-2 w-80 h-80 overflow-hidden'>
 			<Image
-				className='rounded-xl group-hover:opacity-10'
 				src={backgroundImg}
-				alt='/'
+				alt=''
+				className='h-[100%] w-[100%] object-cover object-top'
 			/>
-			<div className='hidden hover:block absolute top-[20%] left-[20%] translate-x-[-20%] translate-y-[-20%]'>
-				<h3 className='text-2xl tracking-wider text-center text-white'>
-					{title}
-				</h3>
-				<p className='pt-2 pb-4 text-center text-white'>{tech}</p>
-				<Link href={projectUrl}>
-					<p className='py-3 text-lg font-bold text-center text-gray-700 bg-white rounded-lg cursor-pointer'>
-						More Info
-					</p>
-				</Link>
-			</div>
+			<h3>{title}</h3>
+			<p>{tech}</p>
+			<button>
+				<Link href={projectUrl}>More info</Link>
+			</button>
 		</div>
 	);
 };
