6330a25cd3f7b27716764ff7c634da996f053100
joakom24 <joakoomussi@gmail.com>
2023-01-14 21:11:36 -0300

    fix styles

--- common/ProjectItem.tsx
+++ common/ProjectItem.tsx
@@ -8,15 +8,22 @@ const ProjectItem = (props: projectItem) => {
 	return (
 		<div className='flex flex-col items-center gap-2 relative overflow-hidden'>
 			<Image
+				className='rounded-xl group-hover:opacity-10'
 				src={backgroundImg}
 				alt='Picture of the author'
 				className='hover:object-bottom cursor-pointer'
 			/>
-			<h3>{title}</h3>
-			<p>{tech}</p>
-			<button>
-				<Link href={projectUrl}>More info</Link>
-			</button>
+			<div className='hidden hover:block absolute top-[20%] left-[20%] translate-x-[-20%] translate-y-[-20%]'>
+				<h3 className='text-2xl tracking-wider text-center text-white'>
+					{title}
+				</h3>
+				<p className='pt-2 pb-4 text-center text-white'>{tech}</p>
+				<Link href={projectUrl}>
+					<p className='py-3 text-lg font-bold text-center text-gray-700 bg-white rounded-lg cursor-pointer'>
+						More Info
+					</p>
+				</Link>
+			</div>
 		</div>
 	);
 };
