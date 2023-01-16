c8a3820df791a61073108adb6a52fb74cd3008a2
mussiDev <joakoomussi@gmail.com>
2023-01-16 18:54:19 -0300

    projects done

--- common/ProjectItem.tsx
+++ common/ProjectItem.tsx
@@ -14,8 +14,10 @@ const ProjectItem = (props: projectItem) => {
 					className='rounded-md hover:object-bottom h-full w-full object-cover object-top transition-all ease-in-out duration-[1400ms]'
 				/>
 			</div>
-			<p className='py-4'>{paragraph}</p>
-			<span>{icon}</span>
+			<p className='py-4 h-24 max-h-24 text-justify text-lg'>{paragraph}</p>
+			<span className='flex justify-center gap-4 shadow-sky-900 p-4 shadow-xl'>
+				{icon}
+			</span>
 			<div className='flex items-center justify-center gap-4 py-6'>
 				<Button title='Demo' href={page} />
 				<Button title='Code' href={github} />
--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,33 +1,78 @@
 import React from "react";
-import ProjectItem from "../../common/ProjectItem";
 import Title from "../../common/Title";
-import foto from "./foto.jpg";
+import weather from "../../image/weather.png";
+import monolandia from "../../image/monolandia.png";
+import petinder from "../../image/petinder.png";
+import ProjectItem from "../../common/ProjectItem";
+import { FaBootstrap, FaCss3, FaHtml5, FaJs, FaReact } from "react-icons/fa";
+import { SiChakraui } from "react-icons/si";
+import { DiMaterializecss } from "react-icons/di";
 
 const Projects = () => {
 	return (
-		<section className='max-w-[1240px] mx-auto py-16 h-screen' id='projects'>
+		<section
+			className='max-w-[1240px] m-auto py-16 px-4 md:h-screen'
+			id='projects'>
 			<Title title='Projects' color='orange-700' />
-			<div className='grid gap-8 md:grid-cols-2'>
+			<div className='grid md:grid-cols-2 gap-8 py-4'>
 				<ProjectItem
-					title='Property Finder'
-					backgroundImg={monolandia}
-					projectUrl='/property'
-					tech='React JS'
+					src={weather}
+					github='https://github.com/MussiDev/ClimaReactAPI'
+					page='https://reactappweather-7c1cb.web.app/'
+					paragraph='Weather Web App consuming the Open Weather App API.'
+					icon={[
+						<FaReact
+							size={50}
+							className='text-cyan-500 hover:scale-125 duration-700'
+						/>,
+						<DiMaterializecss
+							size={50}
+							className='text-fuchsia-300 hover:scale-125 duration-700'
+						/>,
+					]}
 				/>
 				<ProjectItem
-					title='Crypto App'
-					backgroundImg={petinder}
-					projectUrl='/crypto'
-					tech='React JS'
+					src={monolandia}
+					github='https://github.com/MussiDev/MonoLandia'
+					page='https://monolandia-7816e.web.app/'
+					paragraph='Website created in order to inform about the life and history of the Monkeys.'
+					icon={[
+						<FaHtml5
+							size={50}
+							className='text-red-500 hover:scale-125 duration-700'
+						/>,
+						<FaCss3
+							size={50}
+							className='text-blue-500 hover:scale-125 duration-700'
+						/>,
+						<FaJs
+							size={50}
+							className='text-yellow-500 hover:scale-125 duration-700'
+						/>,
+						<FaBootstrap
+							size={50}
+							className='text-purple-500 hover:scale-125 duration-700'
+						/>,
+					]}
 				/>
 				<ProjectItem
-					title='Netflix App'
-					backgroundImg={weather}
-					projectUrl='/netflix'
-					tech='React JS'
+					src={petinder}
+					github='https://github.com/joakom24/Petinder'
+					page='https://petinder-75b0e.web.app/'
+					paragraph='Web application to manage shifts with LocalStorage.'
+					icon={[
+						<FaReact
+							size={50}
+							className='text-cyan-500 hover:scale-125 duration-700'
+						/>,
+						<SiChakraui
+							size={50}
+							className='text-cyan-300 hover:scale-125 duration-700'
+						/>,
+					]}
 				/>
 			</div>
-		</div>
+		</section>
 	);
 };
 
