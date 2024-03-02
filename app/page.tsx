1a0ec3b87307b555ec0df848da8924822e548a10
mussiDev <joakoomussi@gmail.com>
2024-03-02 12:40:06 -0300

    Fixes varios

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,24 @@
-"use client"; // this is a client component
-
-import "../styles/globals.css";
+import {
+	About,
+	Contact,
+	Experience,
+	Footer,
+	Main,
+	Navbar,
+} from "./src/components";
 
 import React from "react";
-import dynamic from "next/dynamic";
-
-const About = dynamic(() => import("./src/components/About/About"), {
-	ssr: false,
-});
-const Main = dynamic(() => import("./src/components/Main/Main"), {
-	ssr: false,
-});
-const Projects = dynamic(() => import("./src/components/Projects/Projects"), {
-	ssr: false,
-});
-const Experience = dynamic(
-	() => import("./src/components/Experience/Experience"),
-	{
-		ssr: false,
-	}
-);
-const Contact = dynamic(() => import("./src/components/Contact/Contact"), {
-	ssr: false,
-});
-const Arrow = dynamic(() => import("./src/common/Arrow"), { ssr: false });
-const Navbar = dynamic(() => import("./src/components/Navbar/Navbar"), {
-	ssr: false,
-});
-const Footer = dynamic(() => import("./src/components/Footer/Footer"), {
-	ssr: false,
-});
 
 const Home = () => {
 	return (
 		<main>
+			{/*
 			<Navbar />
 			<Main />
 			<About />
-			{/*<Projects />*/}
 			<Experience />
 			<Contact />
-			<Arrow />
-			<Footer />
+	/<Footer />*/}
 		</main>
 	);
 };
